55b927ba7122719fe04a1bd9c435f1085728edfe
hashtagyx <c.yongxuan@gmail.com>
2023-08-24 01:56:02 +0800

    Section 6

--- app/page.jsx
+++ app/page.jsx
@@ -1,30 +1,29 @@
 import Link from "next/link";
 import Heading from "@/components/Heading";
+import { getFeaturedReview } from "@/lib/reviews";
 
 // 'use client';
 // import { useEffect } from 'react';
 // default: server component (html rendered on server)
 // client components: rendered on server + client, can use client-side functionality
-export default function HomePage() {
-  //   useEffect(() => {
-  //     window.alert("Welcome to my site!");
-  //   }, []);
+export default async function HomePage() {
   console.log("[HomePage] rendering");
+  const featuredReview = await getFeaturedReview();
   return (
     <>
       <Heading>Indie Gamer</Heading>
       <p className="pb-3">Only the best indie games, reviewed for you.</p>
       <div className="border rounded shadow hover:shadow-xl w-80 sm:w-full bg-white">
-        <Link href="/reviews/stardew-valley" className="flex flex-col sm:flex-row">
+        <Link href={`/reviews/${featuredReview.slug}`} className="flex flex-col sm:flex-row">
           <img
-            src="/images/stardew-valley.jpg"
+            src={featuredReview.image}
             alt=""
             width="320"
             height="180"
             className="rounded-t sm:rounded-l sm:rounded-r-none"
           />
           <h2 className="font-semibold font-orbitron text-center py-1 sm:px-2 sm:flex sm:items-center sm:w-full sm:justify-center">
-            Stardew Valley
+            {featuredReview.title}
           </h2>
         </Link>
       </div>
--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -1,36 +1,32 @@
 import Link from "next/link";
 import Heading from "@/components/Heading";
+import { getReviews } from "@/lib/reviews";
 
-export default function ReviewsPage() {
+export default async function ReviewsPage() {
+  const reviews = await getReviews();
   return (
     <>
       <Heading>Reviews</Heading>
       <p>Here we'll list all the reviews.</p>
-      <ul className="flex flex-col gap-3">
-        <li className="border rounded shadow hover:shadow-xl w-80 bg-white">
-          <Link href="/reviews/hollow-knight">
-            <img
-              src="/images/hollow-knight.jpg"
-              alt=""
-              width="320"
-              height="180"
-              className="rounded-t"
-            />
-            <h2 className="font-semibold font-orbitron text-center py-1">Hollow Knight</h2>
-          </Link>
-        </li>
-        <li className="border rounded shadow hover:shadow-xl w-80 bg-white">
-          <Link href="/reviews/stardew-valley">
-            <img
-              src="/images/stardew-valley.jpg"
-              alt=""
-              width="320"
-              height="180"
-              className="rounded-t"
-            />
-            <h2 className="font-semibold font-orbitron text-center py-1">Stardew Valley</h2>
-          </Link>
-        </li>
+      <ul className="flex flex-wrap flex-row gap-3">
+        {reviews.map(({ slug, title, image }) => {
+          return (
+            <li key={slug} className="border rounded shadow hover:shadow-xl w-80 bg-white">
+              <Link href={`/reviews/${slug}`}>
+                <img
+                  src={image}
+                  alt=""
+                  width="320"
+                  height="180"
+                  className="rounded-t"
+                />
+                <h2 className="font-semibold font-orbitron text-center py-1">
+                  {title}
+                </h2>
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </>
   );
