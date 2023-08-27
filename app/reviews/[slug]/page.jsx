698165c246ef9f0741a73e0120a9ab4461106815
hashtagyx <c.yongxuan@gmail.com>
2023-08-27 20:59:24 +0800

    Deployment

--- app/reviews/[slug]/page.jsx
+++ app/reviews/[slug]/page.jsx
@@ -1,27 +1,30 @@
 import Heading from "@/components/Heading";
-import { getSlugs, getReview } from '@/lib/reviews';
+import ShareLinkButton from "@/components/ShareLinkButton";
+import { getSlugs, getReview } from "@/lib/reviews";
 
 export async function generateStaticParams() {
   const slugs = await getSlugs();
   return slugs.map((slug) => ({ slug }));
 }
 
-export async function generateMetadata({params: { slug }}) {
- const review = await getReview(slug);
+export async function generateMetadata({ params: { slug } }) {
+  const review = await getReview(slug);
   return {
     title: review.title,
-    description: 'Only the best indie games, reviewed for you.',
+    description: "Only the best indie games, reviewed for you.",
   };
-  
-};
+}
 
-export default async function ReviewPage({ params: { slug }}) {
+export default async function ReviewPage({ params: { slug } }) {
   const review = await getReview(slug);
-  console.log('[ReviewPage] rendering:', slug);
+  console.log("[ReviewPage] rendering:", slug);
   return (
     <>
       <Heading>{review.title}</Heading>
-      <p className="italic pb-2">{review.date}</p>
+      <div className="flex gap-3 items-baseline">
+        <p className="italic pb-2">{review.date}</p>
+        <ShareLinkButton />
+      </div>
       <img
         src={review.image}
         alt=""
@@ -29,7 +32,10 @@ export default async function ReviewPage({ params: { slug }}) {
         height="360"
         className="mb-2 rounded"
       />
-      <article dangerouslySetInnerHTML={{ __html: review.body }} className="prose prose-slate max-w-screen-sm text-justify" />
+      <article
+        dangerouslySetInnerHTML={{ __html: review.body }}
+        className="prose prose-slate max-w-screen-sm text-justify"
+      />
     </>
   );
 }
